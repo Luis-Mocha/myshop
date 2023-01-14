d9cc0fb1e7e88b81dba0c9cab05352948e3d781e
Luis-Mocha <120339046+Luis-Mocha@users.noreply.github.com>
2023-01-14 12:40:49 +0100

    Gallery (not working)

--- script.js
+++ script.js
@@ -18,6 +18,7 @@ fetch("./items.json")
 .then(data => console.log(data));
 */
 
+/*Configuro file json*/
 import items from './items.json' assert {type:"json"};
 console.log(items);
 
@@ -28,12 +29,15 @@ const menuCloseButton = document.querySelector('#close-button')
 
 //Apro il menu
 menuButton.addEventListener('click', function () {
-    menuOptions.classList.remove('menu-hidden')
+    menuOptions.classList.toggle('menu-show')
 });
 
-menuCloseButton.addEventListener('click', function() {
-    menuOptions.classList.add('menu-hidden')
-})
+//Chiudo il menu
+menuCloseButton.addEventListener('click', closeMenu)
+//Funzione per chiudere il menu
+function closeMenu() {
+    menuOptions.classList.remove('menu-show')
+};
 
 /*
 $(menuThing).on('click', function(){
@@ -41,3 +45,36 @@ $(menuThing).on('click', function(){
      $(this).toggle(true);
 })
 */
+
+
+/* --GALLERY-- */
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("demo");
+  let captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  captionText.innerHTML = dots[slideIndex-1].alt;
+}
