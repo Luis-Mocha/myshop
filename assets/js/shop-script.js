a8954420614bfcf4eec255ad8fec43181501abda
Luis-Mocha <luismocha97@gmail.com>
2023-02-16 18:25:03 +0100

    typos

--- assets/js/shop-script.js
+++ assets/js/shop-script.js
@@ -80,30 +80,3 @@ loginForm.addEventListener('submit', function (event) {
   //Chiudo la modale
   loginModal.classList.toggle('login-show');
 });
-
-
-
-/* --ABOUT US-- */
-
-function openAbout(aboutName, elmnt, color) {
-  // Default. nascondo tutti gli elementi tabcontent
-  let i, tabcontent, tablinks;
-  tabcontent = document.getElementsByClassName("tabcontent");
-  for (i = 0; i < tabcontent.length; i++) {
-    tabcontent[i].style.display = "none";
-  }
-
-  // elimino il colore background dei tablink
-  tablinks = document.getElementsByClassName("tablink");
-  for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].style.backgroundColor = "";
-  }
-
-  // Mostro il tabcontent specifico
-  document.getElementById(aboutName).style.display = "block";
-
-  // Aggoungo un colore specifico al tablink selezionato
-  elmnt.style.backgroundColor = color;
-} 
-// Rendo il tabcontent che voglio aperto di default
-document.getElementById("defaultOpen").click();
